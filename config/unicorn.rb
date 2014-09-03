836cd63100933f2a98a234edf92fc5d2aa4dbab5
Netanel Moshkovitz <nethanel@yotpo.com>
2014-09-03 18:41:31 +0300

    change config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ timeout 300
 preload_app true
 listen 4567
 
-@app_path = '/Users/netanel/Development/yotpo-workspace/yotpo-monitor'
+@app_path = '/Users/netanel/Development/yotpo-workspace/yotpo-analytics-injection'
 if ENV['RACK_ENV']=="production"
   @app_path = './'
 end
