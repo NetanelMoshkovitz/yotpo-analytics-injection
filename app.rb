e93a41c75fc55fb81a3ddb43b62520e52e2c4a8a
Netanel Moshkovitz <nethanel@yotpo.com>
2014-09-04 11:06:13 +0300

    added reference to purchase

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ class App < Sinatra::Application
                     {:conversion_order =>[
                          {:account_id =>@conversion_order.account_id, :order_id=>@conversion_order.order_id,:amount=>@conversion_order.amount,:currency =>@conversion_order.currency,:reference =>@conversion_order.reference,:created_at=>@conversion_order.created_at.to_date}],
                      :purchase =>[
-                         {:account_id=>@purchase.account_id,:user_email=>@purchase.user_email,:order_id=>@purchase.order_id,:user_name=>@purchase.user_name,:order_date=>@purchase.order_date.to_date,:product_currency=>@purchase.product_currency,:product_sku=>@purchase.product_sku,:product_url=>@purchase.product_url,:product_name=>@purchase.product_name,:product_image=>@purchase.product_image,:product_description=>@purchase.product_description,:product_price=>@purchase.product_price,:created_at=>@purchase.created_at.to_date}]}}.to_json
+                         {:account_id=>@purchase.account_id,:reference =>@purchase.reference,:user_email=>@purchase.user_email,:order_id=>@purchase.order_id,:user_name=>@purchase.user_name,:order_date=>@purchase.order_date.to_date,:product_currency=>@purchase.product_currency,:product_sku=>@purchase.product_sku,:product_url=>@purchase.product_url,:product_name=>@purchase.product_name,:product_image=>@purchase.product_image,:product_description=>@purchase.product_description,:product_price=>@purchase.product_price,:created_at=>@purchase.created_at.to_date}]}}.to_json
     headers  = {:content_type => "application/json"}
     response = RestClient.post "http://analytics.yotpo.com/track", values, headers
 
